f62545cb5182c39faf0e21ab4cc8f5ce1021aa74
jjpoir10 <jp5104@nyu.edu>
2022-11-04 19:21:09 -0400

    Add note array

--- sketch.js
+++ sketch.js
@@ -1,25 +1,23 @@
 let monoSynth;
-let key;
+let notes = ["C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4", "C5"]
 
 function setup() {
     createCanvas(400, 400);
+    let cnv = createCanvas(100, 100);
+    cnv.mousePressed(playSynth);
     monoSynth = new p5.MonoSynth();
   }
   
   function draw() {
     background(220);
-    fill(255);
-    key = rect(0,0,200,200);
-    key.mousePressed(playSynth);
   }
 
   function playSynth() {
     userStartAudio();
 
-    let note1 = C4;
     let velocity = 1;
     let time = 0;
     let dur = 1/6;
 
-    monoSynth.play(note1, velocity, time, dur);
+    monoSynth.play(notes[2], velocity, time, dur);
   }

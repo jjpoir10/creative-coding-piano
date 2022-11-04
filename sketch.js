7a718f4ccadcd870e9042d15b88e74d829def835
jjpoir10 <jp5104@nyu.edu>
2022-11-04 19:05:37 -0400

    Test monoSynth

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
 let monoSynth;
+let key;
 
 function setup() {
     createCanvas(400, 400);
+    monoSynth = new p5.MonoSynth();
   }
   
   function draw() {
     background(220);
     fill(255);
-    rect(0,0,200,200);
+    key = rect(0,0,200,200);
+    key.mousePressed(playSynth);
+  }
+
+  function playSynth() {
+    userStartAudio();
+
+    let note1 = C4;
+    let velocity = 1;
+    let time = 0;
+    let dur = 1/6;
+
+    monoSynth.play(note1, velocity, time, dur);
   }

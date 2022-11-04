93a9356914c8ac4fabd4d72e21c3ad3397891383
jjpoir10 <jp5104@nyu.edu>
2022-11-04 19:44:28 -0400

    Add mouseX if statements

--- sketch.js
+++ sketch.js
@@ -2,14 +2,13 @@ let monoSynth;
 let notes = ["C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4", "C5"]
 
 function setup() {
-    createCanvas(400, 400);
-    let cnv = createCanvas(100, 100);
+    let cnv = createCanvas(400, 200);
     cnv.mousePressed(playSynth);
     monoSynth = new p5.MonoSynth();
   }
   
   function draw() {
-    background(220);
+    background(0);
   }
 
   function playSynth() {
@@ -19,5 +18,10 @@ function setup() {
     let time = 0;
     let dur = 1/6;
 
-    monoSynth.play(notes[2], velocity, time, dur);
+    if(mouseX <= 200) {
+      monoSynth.play(notes[0], velocity, time, dur);
+      }
+        else if(mouseX > 200) {
+      monoSynth.play(notes[12], velocity, time, dur);
+      }
   }

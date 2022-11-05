ae63f6995b78a7c6680819c905523a7bab5aef9f
jjpoir10 <jp5104@nyu.edu>
2022-11-04 20:02:29 -0400

    Test piano keys

--- sketch.js
+++ sketch.js
@@ -2,13 +2,17 @@ let monoSynth;
 let notes = ["C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4", "C5"]
 
 function setup() {
-    let cnv = createCanvas(400, 200);
+    let cnv = createCanvas(displayWidth, displayHeight);
     cnv.mousePressed(playSynth);
     monoSynth = new p5.MonoSynth();
   }
   
   function draw() {
     background(0);
+
+    fill(255);
+    rect(200, 100, 50, 300);
+    rect(251, 100, 50, 300);
   }
 
   function playSynth() {
@@ -18,10 +22,10 @@ function setup() {
     let time = 0;
     let dur = 1/6;
 
-    if(mouseX <= 200) {
+    if(mouseX >= 200 && mouseX < 250 && mouseY >= 100 && mouseY <=400) {
       monoSynth.play(notes[0], velocity, time, dur);
       }
-        else if(mouseX > 200) {
+        else if(mouseX >= 251 && mouseX < 301 && mouseY >= 100 && mouseY <=400) {
       monoSynth.play(notes[12], velocity, time, dur);
       }
   }
